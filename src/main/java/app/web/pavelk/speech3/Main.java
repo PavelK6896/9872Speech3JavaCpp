0171a918831384039635e4954f103ecc9e3f57cf
PavelK6896 <43148722+PavelK6896@users.noreply.github.com>
2021-02-08 22:15:31 +0300

    update one read buf

--- src/main/java/app/web/pavelk/speech3/Main.java
+++ src/main/java/app/web/pavelk/speech3/Main.java
@@ -4,19 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("main");
 
-        JFrame jFrame = new JFrame ();
+        JFrame jFrame = new JFrame();
         jFrame.setBounds(500, 500, 50, 50);
         JButton jButton = new JButton("->");
         SystemTray sT = SystemTray.getSystemTray();
@@ -28,20 +29,27 @@ public class Main {
         jFrame.setUndecorated(true);
         jFrame.setAlwaysOnTop(true);
 
-        ExecutorService es =  Executors.newFixedThreadPool(1);
+        ExecutorService es = Executors.newFixedThreadPool(1);
         Speech speech = new Speech();
 
         es.submit(() -> {
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             AtomicReference<String> text = new AtomicReference<>();
+            AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
             clipboard.addFlavorListener(e -> {
+                if (atomicBoolean.get()) {
+                    atomicBoolean.set(false);
+                    return;
+                }
+
                 String string = null;
+                Transferable contents = clipboard.getContents(new Object());
                 try {
-                    string = (String) clipboard.getData(DataFlavor.stringFlavor);
+                    string = (String) contents.getTransferData(DataFlavor.stringFlavor);
                 } catch (UnsupportedFlavorException | IOException unsupportedFlavorException) {
                     unsupportedFlavorException.printStackTrace();
                 }
-                clipboard.setContents(new StringSelection(string), null);
                 //content
                 System.out.println("-- " + string);
                 text.set(string);
@@ -49,7 +57,11 @@ public class Main {
                 jFrame.setLocation((int) MouseInfo.getPointerInfo().getLocation().getX(),
                         (int) MouseInfo.getPointerInfo().getLocation().getY());
                 jFrame.setVisible(true);
+
+                clipboard.setContents(contents, null);
+                atomicBoolean.set(true);
             });
+
 
             jButton.addActionListener(e -> {
                 jFrame.setVisible(false);
